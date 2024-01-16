70da6fbe990c59d53e5c401d9829c234b8fde05c
Diksha Babhulkar <dikshababhulkar11@gmail.com>
2024-01-16 23:14:36 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,12 +5,6 @@
 
     header("location:login.php");
     exit;
-    // if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
-    //     header("location:login.php");
-    //     exit;
-    // }
-    // else{
-
-    // }
+   
 
 ?>
